88811ff5212beba01616d99deebc9ad69c8666f8
Khomych1004 <khomych100488@gmail.com>
2024-03-24 22:17:02 +0000

    added function to show message when saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,41 @@
 // Getting the current date element
 const $dateEl = $("#currentDay");
 // Getting the element to insert
-const container = $('.container');
-
+const $container = $('.container');
+console.log($container)
 // Array for standard and business hours
 const businessHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+
+function showMessage() {
+    const $messageDiv = $('.messageNone');
+    console.log($messageDiv)
+
+    setTimeout(function () { $messageDiv.toggle("messageShow") }, 2000);
+
+    $messageDiv.toggle("messageNone")
+}
 
 // Saving to local storage
 function saveButtonClick() {
     var $btnId = $(this).attr("id");
     var $text = $("#description_" + $btnId).val();
 
+    showMessage()
+
     saveLocalStorage($btnId, $text)
 }
 
 $(document).ready(function () {
+    const text = 'Appointment Added to'
+    const spanColor = '<span class="colortext">localStorage</span>'
+    const iconCheck = '<i class="fa fa-check" style="color:green"></i>'
+    const textMessage = text + ' ' + spanColor + ' ' + iconCheck
+    const textMessageP = $('<p>').append(textMessage)
+    const textMessageDiv = $('<div>').addClass('messageNone')
+
+    textMessageDiv.append(textMessageP)
+    $container.append(textMessageDiv)
+
 
     // Getting and writing the current date
     $dateEl.text(getDateForPage());
@@ -48,7 +69,7 @@ $(document).ready(function () {
 
         // Append elements to container
         timeBlock.append(hourCol, textAreaCol, saveBtnCol)
-        container.append(timeBlock)
+        $container.append(timeBlock)
         saveBtnCol.append(saveBtn)
     })
 });
