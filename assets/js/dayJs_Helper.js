b15fd8c315e47a6b4f1eca3e45eb1b8cb50e5d1f
Khomych1004 <khomych100488@gmail.com>
2024-03-24 15:33:47 +0000

    added function to display the current date on the page

---
+++ assets/js/dayJs_Helper.js
@@ -0,0 +1,34 @@
+// Returns the current date in the required format
+function getDateForTopOfWebPage() {
+
+    const currentDate = dayjs();
+    const dayOfWeek = currentDate.format('dddd');
+    const currentMonth = currentDate.format('MMMM');
+    const currentDay = currentDate.format('D');
+
+    return dayOfWeek + ", " + currentMonth + " " + currentDay + addSuffix(currentDay)
+}
+
+// Adding an ending to a date
+function addSuffix(day) {
+
+    let dayStr = day.toString();
+
+    const lastDigit = Number(dayStr.charAt(dayStr.length - 1));
+
+    if (day > 10 && day < 14) {
+        return 'th'
+    }
+    else {
+        switch (lastDigit) {
+            case 1:
+                return 'st';
+            case 2:
+                return 'nd';
+            case 3:
+                return 'rd';
+            default:
+                return 'th';
+        }
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function () {
+
+    // Getting the current date element
+    var $dateEl = $("#currentDay");
+
+    // Getting and writing the current date
+    $dateEl.text(getDateForTopOfWebPage());
+
+});
